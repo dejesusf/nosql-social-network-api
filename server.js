fcf62e0e1bbf48e1d24c3b055fbab886ba49d91b
dejesusf <dejesusf@uw.edu>
2023-02-26 00:07:54 -0800

    fixed connection js

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const db = require('./config/connection');
 
-// require model
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// get
-
 
 db.once('open', () => {
   app.listen(PORT, () => {
