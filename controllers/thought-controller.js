093540aa299845a0aa0eb416537bb1042b711a91
dejesusf <dejesusf@uw.edu>
2023-02-25 19:23:03 -0800

    set up controllers files

---
+++ controllers/thought-controller.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require('mongoose');
+const { Thought, User } = require('../models');
+
+// /api/thoughts
+// TODO: GET all thoughts
+
+// TODO: GET a single thought by _id
+
+// TODO: PUT to update thought by _id
+
+// TODO: DELETE to remove a thought by _id
+
+// /api/thoughts/:thoughtId/reactions
+// TODO: POST create a reaction stored in a single thought's reactions array field
+
+// TODO: DELETE to remove a reaction by reactionIds
+
---
+++ controllers/user-controller.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+const { Thought, User } = require('../models');
+
+// /api/users
+// TODO: GET all users
+
+// TODO: GET a single user by its _id and populate thought and friend data
+
+// TODO: POST a new user
+
+// TODO: PUT update user by _id
+
+// TODO: DELETE user by _id
+
+// TODO: BONUS: remove a user's associated thought when deleted
+
+
+// api/users/:userId/friends/:friendId
+// TODO: POST to add a new friend to friend's list
+
+// TODO: DELETE to remove a friend from friend's list
